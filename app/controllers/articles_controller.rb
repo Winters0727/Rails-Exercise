c5c910b7c7da6dc6554c58697cc11ef9defe998d
ChunHwa Park <winters0727@gmail.com>
2020-11-21 21:02:46 +0900

    Update code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,35 @@
 class ArticlesController < ApplicationController
     def new
+      @article = Article.new
+    end
+
+    def index
+        @articles = Article.all
     end
 
     def create
         @article = Article.new(article_params)
 
-        @article.save
-
-        redirect_to @article
+        if @article.save
+          redirect_to @article
+        else
+          render "new"
+        end
         # render plain: params[:article].inspect
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
     end
 
     def show
